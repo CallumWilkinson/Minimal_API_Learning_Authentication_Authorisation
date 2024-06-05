545c75a70adeb0111e175b3ed2742b0048e87ca6
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-06-06 00:41:30 +1000

    added http request for login cookie

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.DataProtection;
+
 namespace Minimal_API_Learning_Authentication_Authorisation
 {
     public class Program
@@ -5,17 +7,27 @@ namespace Minimal_API_Learning_Authentication_Authorisation
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            //microsoft service to encrypt cookies
+            builder.Services.AddDataProtection();
+
             var app = builder.Build();
 
-            app.MapGet("/username", () =>
+            app.MapGet("/username", (HttpContext ctx) =>
             {
-                return "callum";
+                var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+                var payload = authCookie.Split("=").Last();
+                var parts = payload.Split(":");
+                var key = parts[0];
+                var value = parts[1];
+                return value;
             }
                 );
-            //test
+            
 
-            app.MapGet("/login", (HttpContext ctx) =>
+            app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
             {
+                var protector = idp.CreateProtector("auth-cookie");
                 ctx.Response.Headers["set-cookie"] = "auth=usr:callum";
                 return "ok";
             }
