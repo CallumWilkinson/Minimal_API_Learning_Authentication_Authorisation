0b6a188a3c63d2e6a1f0f419966ca208d833f70f
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-06-07 17:31:59 +1000

    im lost in the tutorial at this point

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.DataProtection;
 using System.Runtime.Intrinsics.Arm;
 using System.Security.AccessControl;
+using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 
 namespace Minimal_API_Learning_Authentication_Authorisation
@@ -21,27 +22,32 @@ namespace Minimal_API_Learning_Authentication_Authorisation
 
             var app = builder.Build();
 
-            app.Use((ctx, next) =>
-            {
-                var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
-                var protector = idp.CreateProtector("auth-cookie");
+            //app.Use((ctx, next) =>
+            //{
+            //    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
+            //    var protector = idp.CreateProtector("auth-cookie");
 
-                var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-                var protectedPayload = authCookie.Split("=").Last();
-                var payload = protector.Unprotect(protectedPayload);
-                var parts = payload.Split(":");
-                var key = parts[0];
-                var value = parts[1];
+            //    var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
+            //    var protectedPayload = authCookie.Split("=").Last();
+            //    var payload = protector.Unprotect(protectedPayload);
+            //    var parts = payload.Split(":");
+            //    var key = parts[0];
+            //    var value = parts[1];
+
+            //    var claims = new List<Claim>();
+            //    claims.Add(new Claim(key, value));
+            //    var identity = new ClaimsIdentity(claims);
+            //    ctx.User = new ClaimsPrincipal(identity);
 
                 
 
-                return next();
-            });
+            //    return next();
+            //});
 
             app.MapGet("/username", (HttpContext ctx) =>
 
             {
-                return ctx.User;
+                return ctx.User.FindFirst("usr").Value;
             });
             
 
@@ -56,24 +62,24 @@ namespace Minimal_API_Learning_Authentication_Authorisation
         }
 
 
-        public class AuthService
-        {
-            private readonly IDataProtectionProvider _idp;
-            private readonly IHttpContextAccessor _accessor;
+        //public class AuthService
+        //{
+        //    private readonly IDataProtectionProvider _idp;
+        //    private readonly IHttpContextAccessor _accessor;
 
-            public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
-            {
-                _idp = idp;
-                _accessor = accessor;
-            }
+        //    public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+        //    {
+        //        _idp = idp;
+        //        _accessor = accessor;
+        //    }
 
 
-            public void SignIn()
-            {
-                var protector = _idp.CreateProtector("auth-cookie");
-                _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:callum")}";
-            }
-        }
+        //    public void SignIn()
+        //    {
+        //        var protector = _idp.CreateProtector("auth-cookie");
+        //        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:callum")}";
+        //    }
+        //}
 
         
     }
