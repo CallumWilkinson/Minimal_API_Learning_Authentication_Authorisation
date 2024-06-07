f85c1c30e32d778d2a9b01c567835914649e21c3
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-06-07 17:09:41 +1000

    continuing tutorial

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.DataProtection;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.AccessControl;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Minimal_API_Learning_Authentication_Authorisation
 {
@@ -8,32 +11,70 @@ namespace Minimal_API_Learning_Authentication_Authorisation
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             //microsoft service to encrypt cookies
             builder.Services.AddDataProtection();
+            builder.Services.AddHttpContextAccessor();
+            //is this adding the authservice class i added below?
+            builder.Services.AddScoped<AuthService>();
+        
 
             var app = builder.Build();
 
-            app.MapGet("/username", (HttpContext ctx) =>
+            app.Use((ctx, next) =>
             {
+                var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
+                var protector = idp.CreateProtector("auth-cookie");
+
                 var authCookie = ctx.Request.Headers.Cookie.FirstOrDefault(x => x.StartsWith("auth="));
-                var payload = authCookie.Split("=").Last();
+                var protectedPayload = authCookie.Split("=").Last();
+                var payload = protector.Unprotect(protectedPayload);
                 var parts = payload.Split(":");
                 var key = parts[0];
                 var value = parts[1];
-                return value;
-            }
-                );
+
+                
+
+                return next();
+            });
+
+            app.MapGet("/username", (HttpContext ctx) =>
+
+            {
+                return ctx.User;
+            });
             
 
-            app.MapGet("/login", (HttpContext ctx, IDataProtectionProvider idp) =>
+            app.MapGet("/login", (AuthService auth) =>
             {
-                var protector = idp.CreateProtector("auth-cookie");
-                ctx.Response.Headers["set-cookie"] = "auth=usr:callum";
+                auth.SignIn();
                 return "ok";
-            }
-                );
+            });
 
             app.Run();
+
         }
+
+
+        public class AuthService
+        {
+            private readonly IDataProtectionProvider _idp;
+            private readonly IHttpContextAccessor _accessor;
+
+            public AuthService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+            {
+                _idp = idp;
+                _accessor = accessor;
+            }
+
+
+            public void SignIn()
+            {
+                var protector = _idp.CreateProtector("auth-cookie");
+                _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:callum")}";
+            }
+        }
+
+        
     }
 }
